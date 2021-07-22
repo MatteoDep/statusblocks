5d29e05dd61c741391e170350e72763d52c75ad6
MatteoDep <matteo.dep97@gmail.com>
2021-07-22 18:29:08 +0200

    addeed wtitle

--- blocks.h
+++ blocks.h
@@ -7,13 +7,14 @@ typedef struct {
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"%{l}",	"bspwm_block",		0,	1},
-	{"%{r}",	"sb-news",	0,	6},
-	{"",	"sb-crypto",	0,	13},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-battery",	5,	3},
-	{"",	"sb-internet",	5,	4},
-	{"",	"sb-clock",	0,	2},
+	{"%{l}",  "sb-bspwm",     0,  1},
+	{"",      "sb-wtitle",    0,  1},
+	{"%{c}",  "sb-clock",     0,  2},
+	{"%{r}",  "sb-news",      0,  6},
+	{"",      "sb-crypto",    0,  13},
+	{"",      "sb-volume",    0,  10},
+	{"",      "sb-battery",   5,  3},
+	{"",      "sb-internet",  5,  4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
